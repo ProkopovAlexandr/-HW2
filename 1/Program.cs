a00e5a25c1349fdd75e156b86688d21b3eac52e7
Alexandr <prok0107@mail.ru>
2022-12-04 17:56:53 +0300

    Конечный проект

---
+++ 1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Введите трёхзначное число:");
+string a = Console.ReadLine();
+Console.WriteLine("Вторая цифра: " + a[1]);
---
+++ 2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+string a = Console.ReadLine();
+
+try{
+    Console.WriteLine("Третья цифра: " + a[2]);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
